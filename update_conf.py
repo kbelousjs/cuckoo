3187995d8f296d9e919eeed8c9c106bfa7f17c6b
kbelousjs <kbelouswork@gmail.com>
2019-08-09 12:49:54 +0300

    Update update_conf.py

--- update_conf.py
+++ update_conf.py
@@ -14,11 +14,15 @@ with open('/home/cuckoo/.cuckoo/conf/reporting.conf', 'w') as conf_file:
     if os.environ.get('MONGO_HOST'):
         reporting_cfg.set('mongodb', 'enabled', 'yes')
         reporting_cfg.set('mongodb', 'host', os.environ['MONGO_HOST'])
-        reporting_cfg.set('mongodb', 'username', os.environ['MONGO_INITDB_ROOT_USERNAME'])
-        reporting_cfg.set('mongodb', 'password', os.environ['MONGO_INITDB_ROOT_PASSWORD'])
         
     if os.environ.get('MONGO_TCP_PORT'):
         reporting_cfg.set('mongodb', 'port', os.environ['MONGO_TCP_PORT'])
+    
+    if os.environ.get('MONGO_INITDB_ROOT_USERNAME'):
+        reporting_cfg.set('mongodb', 'username', os.environ['MONGO_INITDB_ROOT_USERNAME'])
+        
+    if os.environ.get('MONGO_INITDB_ROOT_PASSWORD'):
+        reporting_cfg.set('mongodb', 'password', os.environ['MONGO_INITDB_ROOT_PASSWORD'])
 
     reporting_cfg.write(conf_file)
 
